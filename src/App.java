2489b2688378e4abda11f899d1950cc886c0f29a
Lucana <k.ark.scarlet72@gmail.com>
2023-05-16 22:27:36 +0900

    [Finish]三角形の和を求める問題の作成と回答

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("三角形のとある角の角度を求めるプログラムです。");
+        System.out.println("最初に2つ、角度を入力してください。");
+        
+        int radius1 = sc.nextInt();
+        int radius2 = sc.nextInt();
+        int radius3 = 180 - (radius1 + radius2);
+
+        System.out.println("求める角度は" + radius3 + "°です。");
     }
 
 }
