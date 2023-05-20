53900f8eb73880e8aae6c730efdd7fa15d48f11c
Lucana <k.ark.scarlet72@gmail.com>
2023-05-20 18:34:43 +0900

    [Finish]キリの良い数字を出力する問題。まずは入力値を出力できるかの確認。

---
+++ src/D093.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class D093 {
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("ゾロ目の場合、その数字が出力されます。そうでない場合は'No'が出力されます。");
+    System.out.println("4桁の数字を入力してください。");
+
+    int input = sc.nextInt();
+
+    System.out.println(input);
+  }
+}
