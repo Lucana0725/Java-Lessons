717c691e39b0851c6b524fe9c1b929b9d860db03
Lucana <k.ark.scarlet72@gmail.com>
2023-05-20 18:44:50 +0900

    [Finish]入力値の受け取りをString型のnextLine()に変更、問題を解いてみた。

--- src/D093.java
+++ src/D093.java
@@ -5,8 +5,15 @@ public class D093 {
     System.out.println("ゾロ目の場合、その数字が出力されます。そうでない場合は'No'が出力されます。");
     System.out.println("4桁の数字を入力してください。");
 
-    int input = sc.nextInt();
+    // String[] input = sc.nextLine().split("");  // 入力値をString型として受け、一文字ずつに分解。
+    String input = sc.nextLine();
+    String[] nums = input.split("");
+    // System.out.println(Arrays.toString(nums));  // 入力値がちゃんと配列になってるか確認用
 
-    System.out.println(input);
+    if (nums[0].equals(nums[1]) && nums[0].equals(nums[2]) && nums[0].equals(nums[3])) {
+      System.out.println(input);
+    } else {
+      System.out.println("No");
+    }
   }
 }
