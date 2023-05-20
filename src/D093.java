e22afb980f2d3cd7efc5f94be7e8aa3a81b1dd24
Lucana <k.ark.scarlet72@gmail.com>
2023-05-20 19:03:20 +0900

    [Finish]せっかくなので入力値をint型でも受け取ってやってみた。。

--- src/D093.java
+++ src/D093.java
@@ -5,15 +5,36 @@ public class D093 {
     System.out.println("ゾロ目の場合、その数字が出力されます。そうでない場合は'No'が出力されます。");
     System.out.println("4桁の数字を入力してください。");
 
-    // String[] input = sc.nextLine().split("");  // 入力値をString型として受け、一文字ずつに分解。
-    String input = sc.nextLine();
-    String[] nums = input.split("");
-    // System.out.println(Arrays.toString(nums));  // 入力値がちゃんと配列になってるか確認用
+    // 入力値をString型でnextLine()で受け取った場合
+    // // String[] input = sc.nextLine().split("");  // 入力値をString型として受け、一文字ずつに分解。
+    // String input = sc.nextLine();
+    // String[] nums = input.split("");
+    // // System.out.println(Arrays.toString(nums));  // 入力値がちゃんと配列になってるか確認用
 
-    if (nums[0].equals(nums[1]) && nums[0].equals(nums[2]) && nums[0].equals(nums[3])) {
-      System.out.println(input);
+    // if (nums[0].equals(nums[1]) && nums[0].equals(nums[2]) && nums[0].equals(nums[3])) {
+    //   System.out.println(input);
+    // } else {
+    //   System.out.println("No");
+    // }
+
+    // 入力値をint型でnextInt()で受け取った場合
+    int num = sc.nextInt();
+    int[] nums = new int[4];
+    for (int i = 0; i < 4; i++) {
+      nums[i] = num % 10;
+      num /= 10;
+      // System.out.println("nums[" + i + "]" + nums[i]);  // 確認用出力
+    }
+
+    if (nums[0] == nums[1] && nums[0] == nums[2] && nums[0] == nums[3]) {
+      System.out.println(num);
     } else {
       System.out.println("No");
     }
+    // if (nums[0] == nums[3]) {
+    //   System.out.println("Yes");
+    // } else {
+    //   System.out.println("No");
+    // }
   }
 }
