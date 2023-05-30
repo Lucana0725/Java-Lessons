4fd9a7904ea6d06dff03790035805c06838314ad
Lucana <k.ark.scarlet72@gmail.com>
2023-05-30 22:51:25 +0900

    [Finish]LEET文字を連想配列を作成して保持した

--- src/C016.java
+++ src/C016.java
@@ -7,7 +7,21 @@ public class C016 {
     System.out.println("好きな英単語を入力してください。");
     
     String input = sc.nextLine();  // 文字列の入力の受け取り
+    // System.out.println(input);  // 確認用出力
+    String[] letters = input.split("");  // input文字列を一文字ずつ分解、配列lettersへ。
 
-    System.out.println(input);
+    HashMap<String, String> hmap = new HashMap<String, String>();  // LEET文字を連想配列で格納
+    // 以下、LEET文字の元文字(key)と変換後文字(value)
+    hmap.put("A", "4");
+    hmap.put("E", "3");
+    hmap.put("G", "6");
+    hmap.put("I", "1");
+    hmap.put("O", "0");
+    hmap.put("S", "5");
+    hmap.put("Z", "2");
+
+    // System.out.println(hmap.get("A"));  // とりあえず連想配列の作成と出力は期待通りできている。
+
+
   }
 }
