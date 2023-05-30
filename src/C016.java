dc1a42a772b3b1c75f307d4c1debf45b53824958
Lucana <k.ark.scarlet72@gmail.com>
2023-05-30 22:39:05 +0900

    [Finish]C016問題の作成、入力文字の受け取り確認

---
+++ src/C016.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class C016 {
+  public static void main (String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("入力された文字列をLEET文字列に変換します。");
+    System.out.println("好きな英単語を入力してください。");
+    
+    String input = sc.nextLine();  // 文字列の入力の受け取り
+
+    System.out.println(input);
+  }
+}
