af6c5a8f44c0a840ff3d480c253348cd60e00b5d
Lucana <k.ark.scarlet72@gmail.com>
2023-06-28 00:34:31 +0900

    [Finish]入力回数、'y'or'n'を入力させるよう実装

--- src/C130.java
+++ src/C130.java
@@ -1,6 +1,16 @@
 import java.util.*;
 public class C130 {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("解く問題集の問題数を入力してください");
+    int input_times = sc.nextInt();
+
+    sc.nextLine();  // 入力回数の改行文字の読み飛ばし
+
+    System.out.println("解けた問題は「y」を、解けなかった問題は「n」を、半角スペース区切りで入力してください。");
+    String result = sc.nextLine();
+
     
+
   }
 }
